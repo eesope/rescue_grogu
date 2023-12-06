84695422309c03e9804c93f724007c12ae7c49e8
Saeyoung Park <spark363@my.bcit.ca>
2023-12-06 09:38:53 -0800

    Update to character setting in default_settings.py

--- default_settings.py
+++ default_settings.py
@@ -1,5 +1,5 @@
 """
-Add a doc string
+When initiate game, makes a map and a character.
 """
 
 import random
@@ -7,7 +7,7 @@ import random
 
 def make_board():
     """
-    Take two numbers and create the dictionary of rows * column keys and each descriptive string value.
+    Create the dictionary of rows * column keys and each descriptive string value.
 
     :postcondition: dictionary that contains (rows, columns) keys as tuple that contains a set of coordinates
     :return: dictionary that has keys as a set of coordinates and values as a short string description
@@ -15,7 +15,8 @@ def make_board():
 
     room_values = ["Mandalore Imperial base", "Lush Veshok Tree Forests", "The Armorer's Space", "Shipyards",
                    "Warehouse", "The Living Waters", "Ossus", "Bescar Shop", "Cafe"]
-    room_events = {}
+    merit_events = {}
+    demerit_events = {}
     lv_up_room = ["Level up dungeon"]
 
     board = {}
@@ -30,6 +31,19 @@ def make_board():
 
 
 print(make_board())
+
+
+def make_character():
+    """
+    Create a dictionary that contains the information of X, Y coordinates of character, and its current HP.
+
+    :postcondition: dictionary that contains information of X, Y coordinates of character, and current HP
+
+    >>> make_character()
+    {'X-coordinate': 0, 'Y-coordinate': 0, 'Current HP': 5}
+    """
+
+    return {"X-coordinate": 0, "Y-coordinate": 0, "Current HP": 5, "Skill set": []}
 
 
 # def main():
